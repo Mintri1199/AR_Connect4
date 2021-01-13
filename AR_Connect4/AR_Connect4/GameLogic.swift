405e7d291568eae229c44ba8633869b485b6343e
Jackson Ho <mintri1199@gmail.com>
2021-01-13 10:25:32 -0500

    [Modified&Fixed] added vertical check method and fixed some logic bugs

--- AR_Connect4/AR_Connect4/GameLogic.swift
+++ AR_Connect4/AR_Connect4/GameLogic.swift
@@ -9,112 +9,153 @@
 import Foundation
 
 enum CellState {
-    case red, blue, empty
+  case red, blue, empty
 }
 
 // Standard board size 7 x 6
 
 class Game {
-    var board = Array(repeating: Array(repeating: CellState.empty, count: 7), count: 6)
-    var winner = CellState.empty
-    init() {
-    }
-
-    // Steps to play
-    // 1. One player choose a col to place their cell in, the cell will always go to the bottom
-    //    and stack on top of an already present cell.
-    // 2. Check whether the player has won or not based on where they placed their cell.
-    // 3. Repeat Step 1 for the other player.
-    // 4. If neither player has won and the board has been filled up, then it forces a draw.
-
+  var board = Array(repeating: Array(repeating: CellState.empty, count: 7), count: 6)
+  var winner = CellState.empty
+  init() {
+  }
+  
+  // Steps to play
+  // 1. One player choose a col to place their cell in, the cell will always go to the bottom
+  //    and stack on top of an already present cell.
+  // 2. Check whether the player has won or not based on where they placed their cell.
+  // 3. Repeat Step 1 for the other player.
+  // 4. If neither player has won and the board has been filled up, then it forces a draw.
+  
+  
+  /// Win Condition: The player wins when they achieve 4 or more consecutive cell of their own color
+  /// on the board. The directions can be vertical, horizontal, and diagonal.
+  
+  // Place the player's cell in a column of the board
+  func place(column: Int, color: CellState) {
+    let rowEndIndex = board.endIndex - 1
     
-    /// Win Condition: The player wins when they achieve 4 or more consecutive cell of their own color 
-    /// on the board. The directions can be vertical, horizontal, and diagonal.
-
-    // Place the player's cell in a column of the board
-    func place(column: Int, color: CellState) {
-        let rowEndIndex = board.endIndex - 1
-
-        for i in stride(from: rowEndIndex, to: 0, by: -1) {
-            if board[i][column] == .empty {
-                board[i][column] = color
-                break
-            }
-        }
+    for i in stride(from: rowEndIndex, to: -1, by: -1) {
+      if board[i][column] == .empty {
+        board[i][column] = color
+        break
+      }
     }
-
-    func checkForWin(col: Int, row: Int, count: Int=1, color: CellState, win: Bool=false) {
-        // Base Case cell of current position is empty or doesn't not match the argument's color
-        if board[row][col] != color {
-            return 
-        }
-
-        // check for horizontal win
-        if checkForHozintal(row: row, col: col, color: color) {
-            winner = color
-            print("\(color) win")
-            return
-        }
-
-        // check for vertical win
-        // check for diagonal win
-
-        return
+  }
+  
+  func checkForWin(col: Int, row: Int, count: Int=1, color: CellState, win: Bool=false) {
+    // Base Case cell of current position is empty or doesn't not match the argument's color
+    if board[row][col] != color {
+      return
     }
-
-    private func checkForHozintal(row: Int, col: Int, color: CellState) -> Bool {
-        var count = 0
-        var cell = board[row][col]
-        var colPosition = col
-
-        repeat {
-            count += 1
-            colPosition -= 1
-            if colPosition < 0 {
-                break
-            }
-            cell = board[col][colPosition - 1]
-        } while cell == color && count < 4
-
-        cell = board[row][col + 1]
-        colPosition = row
-        
-        while cell == color && count < 4 {
-            count += 1
-            colPosition += 1
-        }
-
-        return count == 4
+    
+    // check for horizontal win
+    if checkForHorizontal(row: row, col: col, color: color) {
+      winner = color
+      print("Horizontal win")
+      return
     }
-
-    func printGameState() {
-        var tempBoard: [[Int]] = []
-        for row in board {
-            let tempRow: [Int] = row.map { cell in 
-                switch cell {
-                    case .empty:
-                        return 0
-                    case .red: 
-                        return 1
-                    case .blue:
-                        return 2
-                }
-            }
-            tempBoard.append(tempRow)
-        }
-
-        tempBoard.forEach { print($0) }
-         
+    
+    // check for vertical win
+    if checkForVertical(row: row, col: col, color: color) {
+      winner = color
+      print("Vertical win")
+      return
     }
-
+    
+    
+    // check for diagonal win
+    
+    return
+  }
+  
+  func checkForHorizontal(row: Int, col: Int, color: CellState) -> Bool {
+    var count = 0
+    var cell = board[row][col]
+    var colPosition = col
+    
+    repeat {
+      count += 1
+      colPosition -= 1
+      if colPosition < 0 {
+        break
+      }
+      cell = board[row][colPosition]
+    } while cell == color && count < 4
+  
+    cell = board[row][col]
+    colPosition = col
+    
+    while cell == color && count < 4 {
+      colPosition += 1
+      if colPosition > 6 {
+        break
+      }
+      count += 1
+      cell = board[row][colPosition]
+    }
+    
+    return count == 4
+  }
+  
+  func checkForVertical(row: Int, col: Int, color: CellState) -> Bool {
+    var count = 0
+    var rowPosition = row
+    var cell = board[row][col]
+    
+    repeat {
+      count += 1
+      rowPosition -= 1
+      if rowPosition < 0 {
+        break
+      }
+      cell = board[rowPosition][col]
+    } while cell == color && count < 4
+    
+    cell = board[row][col]
+    rowPosition = row
+    
+    while cell == color && count < 4 {
+      rowPosition += 1
+      if rowPosition > 5 {
+        break
+      }
+      count += 1      
+      cell = board[rowPosition][col]
+    }
+    
+    return count == 4
+  }
+  
+  func printGameState() {
+    var tempBoard: [[Int]] = []
+    for row in board {
+      let tempRow: [Int] = row.map { cell in
+        switch cell {
+          case .empty:
+            return 0
+          case .red:
+            return 1
+          case .blue:
+            return 2
+        }
+      }
+      tempBoard.append(tempRow)
+    }
+    
+    tempBoard.forEach { print($0) }
+    
+  }
+  
 }
 
 
 let game = Game()
 game.place(column: 0, color: .red)
 game.place(column: 0, color: .blue)
-game.place(column: 1, color: .red)
-game.place(column: 3, color: .red)
-game.place(column: 2, color: .red)
+game.place(column: 0, color: .red)
+game.place(column: 0, color: .red)
+game.place(column: 0, color: .red)
+game.place(column: 0, color: .red)
 game.printGameState()
-game.checkForWin(col: 0, row: 5, color: .red)
+game.checkForWin(col: 0, row: 3, color: .red)
--- AR_Connect4/AR_Connect4Tests/GameLogicTest.swift
+++ AR_Connect4/AR_Connect4Tests/GameLogicTest.swift
@@ -10,44 +10,73 @@ import XCTest
 @testable import AR_Connect4
 
 class GameLogicTest: XCTestCase {
-    let gameInstance = Game()
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  private let gameInstance = Game()
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testPlaceMethod() {
+    // Placed cell should be at the end of the column array
+    gameInstance.place(column: 0, color: .red)
+    
+    // The other indexes should contains CellState.empty except for the last one
+    for col in 1 ... 5 {
+      XCTAssertEqual(gameInstance.board[5][col], CellState.empty)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    // The placed cell should be at the end of the column array
+    XCTAssertEqual(gameInstance.board[5][0], CellState.red)
+    
+    // Placed cell should be on top of another previously present cell on the same column
+    gameInstance.place(column: 0, color: .blue)
+    
+    // The other indexes should contains CellState.empty except for the last two
+    for i in 1 ... 5 {
+      XCTAssertEqual(gameInstance.board[4][i], CellState.empty)
     }
-
-    func testPlaceMethod() {
-        // Placed cell should be at the end of the column array
-        gameInstance.place(column: 0, color: .red)
-        let endIndex = gameInstance.board[0].count
-
-        // The other indexes should contains CellState.empty except for the last one
-        for i in 0 ..< endIndex - 1 {
-            XCTAssertEqual(gameInstance.board[0][i], CellState.empty)
-        }
-        // The placed cell should be at the end of the column array
-        XCTAssertEqual(gameInstance.board[0][endIndex - 1], CellState.red)
-
-        // Placed cell should be on top of another previously present cell on the same column
-        gameInstance.place(column: 0, color: .blue)
-
-        // The other indexes should contains CellState.empty except for the last two
-        for i in 0 ..< endIndex - 2 {
-          XCTAssertEqual(gameInstance.board[0][i], CellState.empty)
-        }
-        // The placed cell should be at the end of the column array
-        XCTAssertEqual(gameInstance.board[0][endIndex - 2], CellState.blue)
+    // The placed cell should be at the end of the column array
+    XCTAssertEqual(gameInstance.board[4][0], CellState.blue)
+  }
+  
+  func testHorizontalMethod() {
+    // Test the method to detect horizontal win condition
+    gameInstance.place(column: 0, color: .red)
+    gameInstance.place(column: 1, color: .red)
+    gameInstance.place(column: 3, color: .red)
+    gameInstance.place(column: 2, color: .red)
+    
+    XCTAssertTrue(gameInstance.checkForHorizontal(row: 5, col: 1, color: .red))
+    
+    gameInstance.place(column: 6, color: .red)
+    XCTAssertFalse(gameInstance.checkForHorizontal(row: 5, col: 6, color: .red))
+  }
+  
+  func testVerticalMethod() {
+    // Test the method to detect vertical win condition
+    gameInstance.place(column: 0, color: .red)
+    gameInstance.place(column: 0, color: .red)
+    gameInstance.place(column: 0, color: .red)
+    gameInstance.place(column: 0, color: .red)
+    
+    XCTAssertTrue(gameInstance.checkForVertical(row: 4, col: 0, color: .red))
+    
+    gameInstance.place(column: 1, color: .red)
+    gameInstance.place(column: 1, color: .red)
+    gameInstance.place(column: 1, color: .blue)
+    gameInstance.place(column: 1, color: .red)
+    gameInstance.place(column: 1, color: .red)
+    XCTAssertFalse(gameInstance.checkForVertical(row: 4, col: 0, color: .blue))
+  }
+  
+  func testPerformanceExample() throws {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  }
 }
 
